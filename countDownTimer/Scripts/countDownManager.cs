9df5504ebe6a2fb145dfd070901f710600435562
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-15 21:56:01 +0200

    Add files via upload

---
+++ countDownTimer/Scripts/countDownManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countDownManager : MonoBehaviour {
+    Text timerText;
+    private float timerNumber;
+
+	// Use this for initialization
+	void Start () {
+        timerNumber = 60f;
+        timerText = GameObject.Find("timer").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerNumber -= Time.deltaTime;
+        timerText.text = timerNumber.ToString("00.00");
+
+        if(timerNumber<=0)
+        {
+            //do something
+        }
+	}
+}
