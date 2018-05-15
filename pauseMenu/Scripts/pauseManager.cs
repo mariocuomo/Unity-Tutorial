784c4f0d6185964ac66c8af720e9a5d57062d518
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-15 10:45:25 +0200

    Add files via upload

---
+++ pauseMenu/Scripts/pauseManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseManager : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            hideOrShow();
+
+    }
+
+    public void hideOrShow()
+    {
+        if (pauseMenu.activeInHierarchy == true)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void quit()
+    {
+        //Application.LoadLevel("nameOfMainMenu");
+        Application.Quit();
+    }
+}
