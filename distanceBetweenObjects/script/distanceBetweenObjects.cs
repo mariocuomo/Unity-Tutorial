7a0a62b0569ecce5431db969cdbdfcf85e75e891
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-19 21:07:33 +0200

    Add files via upload

---
+++ distanceBetweenObjects/script/distanceBetweenObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class distanceBetweenObjects : MonoBehaviour {
+    public GameObject object1;
+    public GameObject object2;
+    public Text TextDistance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TextDistance.text = Vector3.Distance(object1.transform.position, object2.transform.position).ToString();
+    }
+}
