f2ff80c520eb60c86c7acd783c2c54ea60a3af74
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-22 21:06:16 +0200

    Add files via upload

---
+++ collectCoins/scripts/collectCoins.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collectCoins : MonoBehaviour {
+    int counter = 0;
+    public Text counterText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (counter < 3)
+            counterText.text = "COINS: " + counter.ToString();
+        else
+            counterText.text = "GREAT WORK PIGGY BANK";
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "coin")
+        {
+            counter++;
+            Destroy(other.gameObject);
+        }
+    }
+}
