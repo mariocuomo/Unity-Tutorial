8795c4fea2dd0990c03e1e45bc691cf07e9ff917
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-24 14:38:01 +0200

    Add files via upload

---
+++ saveBetweenSessions/scripts/saveValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class saveValue : MonoBehaviour {
+    public Text lastText;
+    public Text newText;
+
+	// Use this for initialization
+	void Start () {
+        lastText.text = PlayerPrefs.GetString("word");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerPrefs.SetString("word", newText.text);
+	}
+}
