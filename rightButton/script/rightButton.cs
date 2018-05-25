175ac3b47728d2418df75a83b055ea268f0a9aa8
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-25 21:00:48 +0200

    Add files via upload

---
+++ rightButton/script/rightButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rightButton : MonoBehaviour {
+
+    Text textWritten;
+
+	// Use this for initialization
+	void Start () {
+        textWritten = GameObject.Find("detectionText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClick()
+    {
+        if (this.gameObject.name == "rightButton")
+            textWritten.text ="YOU HAVE CLICKED THE RIGHT BUTTON";
+        if (this.gameObject.name == "wrongButton")
+            textWritten.text = "YOU HAVE CLICKED THE WRONG BUTTON";
+    }
+}
