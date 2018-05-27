c116050e2f25893cac797495c9f2cc86ee59866b
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-27 18:22:46 +0200

    Add files via upload

---
+++ spawnObjects/script/spawnObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spawnObjects : MonoBehaviour {
+    public GameObject prefabSpawnObject;
+    float timer;
+    public Sprite[] sprites;
+    float min;
+    float max;
+    Vector2 objectPosition;
+    GameObject Canvas;
+    GameObject lastObject;
+
+	// Use this for initialization
+	void Start () {
+        timer = 3f;
+        Canvas = GameObject.Find("Canvas");
+        min = 40;
+        max = 900;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            objectPosition = new Vector2(Random.Range(min, max), Random.Range(min, max));
+            lastObject = Instantiate(prefabSpawnObject, objectPosition, prefabSpawnObject.transform.rotation, Canvas.transform);
+            lastObject.GetComponent<Image>().sprite = (Sprite)sprites.GetValue(Random.Range(0, 4));
+            timer = 3f;
+        }
+	}
+}
