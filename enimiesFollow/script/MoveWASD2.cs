2f6c16baefa1c78f3c405c04ac03feba44cbf0d4
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-28 19:46:15 +0200

    Add files via upload

---
+++ enimiesFollow/script/MoveWASD2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWASD2 : MonoBehaviour
+{
+    float speed = 100.0f;
+    Vector3 move;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position += move * speed * Time.deltaTime;
+
+    }
+}
---
+++ enimiesFollow/script/enemiesFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemiesFollow : MonoBehaviour {
+
+    public Transform player;
+    float speed;
+    float distanceEnemy;
+    float distance;
+
+	// Use this for initialization
+	void Start () {
+        speed = 5f;
+        distanceEnemy = 200f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if(distance < distanceEnemy)
+        {
+            transform.position = Vector2.Lerp(transform.position, player.transform.position, speed/1000);
+        }
+	}
+
+}
