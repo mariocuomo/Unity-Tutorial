65750c247e39100add1adfb6d2cdfe537b6dc40a
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-29 21:25:57 +0200

    Add files via upload

---
+++ ads/script/ads.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class ads : MonoBehaviour {
+
+    Text timerText;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        Advertisement.Initialize("yourIDNumber");
+        timer = 5f;
+        timerText = GameObject.Find("countdown timer").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        timerText.text = timer.ToString("0");
+
+        if (timer <= 0)
+        {
+            Advertisement.Show("video");
+            timer = 5f;
+        }
+	}
+}
