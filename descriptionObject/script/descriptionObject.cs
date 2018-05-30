640c484a5189e43ee29a3e3e048e1c03a9a85467
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-30 21:51:43 +0200

    Add files via upload

---
+++ descriptionObject/script/descriptionObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class descriptionObject : MonoBehaviour {
+    Text descriptionText;
+    int step;
+
+	// Use this for initialization
+	void Start () {
+        descriptionText = GameObject.Find("infoText").GetComponent<Text>();
+        step = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (step)
+        {
+            case 1:
+                descriptionText.text = "FIND SOMETHING";
+                break;
+            case 2:
+                descriptionText.text = "PRESS [E]";
+                if (Input.GetKeyDown(KeyCode.E))
+                    step = 3;
+                break;
+            case 3:
+                descriptionText.text = "IT IS WINE!";
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        step = 2;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        step = 1;
+    }
+}
