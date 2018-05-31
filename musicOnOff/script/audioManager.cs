a421c87ccd2d10a4e28e9e7e6d554cebe2f644e1
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-05-31 18:03:01 +0200

    Add files via upload

---
+++ musicOnOff/script/audioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioManager : MonoBehaviour {
+    bool musicActive;
+    AudioSource music;
+    Text audioText;
+    public Sprite musicOn;
+    public Sprite musicOff;
+    Image musicImage;
+
+	// Use this for initialization
+	void Start () {
+        musicActive = true;
+        music = GameObject.Find("Canvas").GetComponent<AudioSource>();
+        audioText = GameObject.Find("soundText").GetComponent<Text>();
+        musicImage = GameObject.Find("soundIcon").GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (musicActive == true)
+            {
+                musicActive = false;
+                music.enabled = false;
+                audioText.text = "MUSIC OFF";
+                musicImage.sprite = musicOff;
+            }
+            else
+            {
+                musicActive = true;
+                music.enabled = true;
+                audioText.text = "MUSIC ON";
+                musicImage.sprite = musicOn;
+            }
+        }
+	}
+}
