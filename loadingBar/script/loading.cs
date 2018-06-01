aa91822855d1e4d4b9b7acdc82dbf2964d5ff0e3
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-06-01 23:45:03 +0200

    Add files via upload

---
+++ loadingBar/script/loading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loading : MonoBehaviour {
+    float timer;
+    Scrollbar scrollbar;
+    Text loadingText;
+
+	// Use this for initialization
+	void Start () {
+        timer = 5f;
+        scrollbar = GameObject.Find("loadingBar").GetComponent<Scrollbar>();
+        loadingText = GameObject.Find("percentageText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        scrollbar.size = timer / 5;
+        loadingText.text = (timer / 5 * 100).ToString("00") + "%";
+	}
+}
