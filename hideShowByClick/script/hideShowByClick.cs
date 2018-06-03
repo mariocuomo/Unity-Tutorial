cecbfa599daed90302a687cb8d6a19f3dff6d6ed
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-06-03 21:22:22 +0200

    Add files via upload

---
+++ hideShowByClick/script/hideShowByClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hideShowByClick : MonoBehaviour {
+    Image ghost;
+
+	// Use this for initialization
+	void Start () {
+        ghost = GameObject.Find("GhostImage").GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClick()
+    {
+        if (ghost.enabled == true)
+            ghost.enabled = false;
+        else
+            ghost.enabled = true;
+    }
+}
