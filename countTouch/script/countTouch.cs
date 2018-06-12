8e06723f13720c8b9a3f2e6434621a80d1d42801
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-06-12 21:28:14 +0200

    Add files via upload

---
+++ countTouch/script/countTouch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countTouch : MonoBehaviour {
+    int count;
+    Text countText;
+
+	// Use this for initialization
+	void Start () {
+        count = 0;
+        countText = GameObject.Find("countText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countText.text = count.ToString();
+	}
+
+    public void OnClick()
+    {
+        count++;
+    }
+}
