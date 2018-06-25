74752bd7fbcbdc2f5773ace6c13b63851496e616
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2018-06-25 21:21:31 +0200

    Add files via upload

---
+++ credits/scripts/credits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class credits : MonoBehaviour {
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 20f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            Application.Quit();
+	}
+}
